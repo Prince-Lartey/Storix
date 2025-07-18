6f3df2ac26910997aae7f7e6d94fafa8ec8644e6
Prince Lartey <lartey.princ@gmail.com>
2025-07-18 12:25:59 +0000

    still figuring out the bug...

--- app/(dashboard)/dashboard/settings/roles/update/[id]/page.tsx
+++ app/(dashboard)/dashboard/settings/roles/update/[id]/page.tsx
@@ -3,16 +3,22 @@ import React from "react";
 import RoleForm from "@/components/Forms/RoleForm";
 import { getRoleById } from "@/actions/roles";
 import NotFound from "@/app/not-found";
+import { getAuthenticatedUser } from "@/config/useAuth";
 
 export default async function page({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
-  const id = (await params).id;
-  const { data } = await getRoleById(id);
-  if (!id || !data) {
-    return NotFound();
-  }
-  return <RoleForm editingId={id} initialData={data} />;
+    const id = (await params).id;
+    const user = await getAuthenticatedUser();
+    const orgId = user.orgId;
+    const { data } = await getRoleById(id);
+    if (!id || !data) {
+        return NotFound();
+    }
+  
+    return (
+        <RoleForm editingId={id} initialData={data} orgId={orgId}/>
+    )
 }
